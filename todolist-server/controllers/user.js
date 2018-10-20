596d98c8308763591f3147550d1761478295cbc0
0zoo <e0zoo94@gmail.com>
2018-10-20 16:31:50 +0900

    181020_2

--- todolist-server/controllers/user.js
+++ todolist-server/controllers/user.js
@@ -4,7 +4,46 @@ const bcrypt = require('bcrypt')
 
 const Joi = require("koa-joi-router").Joi;
 
+class ClientError extends Error {
+    constructor(message) {
+      super(message);
+      this.status = 400;
+    }
+  }
+
 // Validator - Joi validator
+
+// User - CRUD
+// Task - CRUD
+// GTD
+
+// 함수
+//  동기: 결과가 바로 반환된다.
+//  비동기
+//    1. 인자로 callback을 받는다.
+//    2. 반환타입이 Promise<T> 이다.
+
+// module.exports.postUser = postUser;
+// 비밀번호 조건 => Regex(정규 표현식)
+//  1) 6글자 이상
+//  2) 반드시 영문자 + 숫자
+//  3) 특수 문자
+//  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{6,}$/
+
+// HTTP 통신은 비 암호화 통신입니다.
+//  => 반드시 HTTPS 를 이용해야 합니다.
+
+// 암호화
+//  1. 단방향(비밀번호, 주민등록번호) - Hash
+//     hello -> sdjklajldjalksdjak!2123
+//  2. 양방향(RSA)
+//     인증서 기반 암호화(RSA)
+//         '공개키'  '비공개키'  
+//  공개키 : 암호화 할 때 사용한다.
+// 비공개키 : 암호를 풀 때 사용한다.
+
+// bcrypt 모듈을 이용하면 됩니다.
+//  SHA1, SHA256, SHA512, SHA1024
 
 // module.exports.postUser = postUser;
 const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/
@@ -20,6 +59,7 @@ const postUser = {
     type: "json",
 },
     async handler(ctx) {
+        /*
 
         // 1. 해당하는 사용자가 이미 존재하는지 확인
         const {
@@ -33,11 +73,6 @@ const postUser = {
 
         if(!_.isNil(exist)){
             throw new ClientError("Already exist email")
-            /*
-            const error = new Error("Already exist email")
-            error.status = 400;
-            throw error
-            */
         }
         // 2. 비밀번호를 암호화해서 저장
         const passwordHash = await bcrypt.hash(password, 10);
@@ -59,6 +94,11 @@ const postUser = {
         // 2.
         // await user.save();
         // ctx.body = user
+
+        */
+
+        // 위의 로직은 모델이 처리해주게 해줘야 한다.
+        ctx.body = await User.signUp(ctx.request.body);
     }
 };
 
--- todolist-server/models/User.js
+++ todolist-server/models/User.js
@@ -1,5 +1,13 @@
+const _ = require("lodash");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+
+const {
+  ClientError,
+} = require("../error");
 
 const User = new Schema({
   name: {
@@ -14,12 +22,48 @@ const User = new Schema({
   },
 });
 
-// 몽구스에서는 모델에 새로운 기능을 추가할 수 있다.
+User.statics.findOneByEmail = async function(email){
+  // return User.findOne({
+  // User 대신 this로 사용해야 한다.
+  return this.findOne({
+    email
+  }).exec();
+};
+
+// Mongoose 에서는 model에 새로운 기능을 추가할 수 있습니다.
+// signUp
+// User.signUp: static method
+// user.signUp: instance method
 // static으로 만들어야된다. 유저가 만들어지기 전이기 때문에
 
 // 여기서는 람다 금지
 User.statics.signUp = async function(data){
 
+  const{
+    email,
+    password,
+  } = data;
+
+  const exist = await this.findOneByEmail(email);
+
+  if (!_.isNil(exist)) {
+    throw new ClientError("Already exist email")
+  }
+
+  const passwordHash = await bcrypt.hash(password,saltRounds);
+  
+  // new User()로 하면 안됨!! 주의
+  const user = new this({
+    ...data,
+    password: passwordHash,
+  });
+
+  await user.save();
+
+  const ret = user.toObject();
+  delete ret.password;
+
+  return ret;
 };
 
 module.exports = mongoose.model("User", User);
